ddd7f595b62574c1a6afac8469d461ee3413ef44
Abhishek Singh <131707739+AbhishekIISERB@users.noreply.github.com>
2026-09-14 11:57:28 +0530

    Add: pow-x-n - submission-2

---
+++ Data Structures & Algorithms/pow-x-n/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long exp = n;
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+        double product = 1;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                product *= x;
+            }
+            x *= x;
+            exp /= 2;
+        }
+        return product;
+    }
+}
